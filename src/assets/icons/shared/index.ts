e21d1e3247b7aab53a5fd3fc9241f7e8d161e080
sossost <shagrat8546@gmail.com>
2024-09-14 17:11:38 +0900

    init

--- src/assets/icons/shared/index.ts
+++ src/assets/icons/shared/index.ts
@@ -1,4 +1,4 @@
 export { default as Lock } from "./lock.svg";
-export { default as UnLock } from "./unlock.svg";
+export { default as UnLock } from "./unLock.svg";
 export { default as Photo } from "./photo.svg";
 export { default as DeletePhoto } from "./deletePhoto.svg";
