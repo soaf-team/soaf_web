95ecd640c8ea6b24c09be114795a515be7b7f63c
sossost <shagrat8546@gmail.com>
2024-09-18 13:15:58 +0900

    feat : 앱으로 부터 토큰 받는 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,33 @@ import { useEffect, useState } from 'react';
 import { ProviderGroup } from './providers';
 import { Stack } from './stackflow';
 import { worker } from './mocks/browser';
+import { axiosBase } from './apis';
+import { log } from './utils';
 
 if (import.meta.env.MODE === 'production') {
 	console.log = () => {};
 }
+
+// @ts-expect-error
+document.addEventListener('message', (event: MessageEvent) => {
+	try {
+		const data =
+			typeof event.data === 'string' ? JSON.parse(event.data) : event.data;
+		const accessToken = data?.accessToken;
+		if (accessToken) {
+			log(accessToken);
+			axiosBase.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+		}
+	} catch (error: unknown) {
+		log(error);
+	}
+});
 
 function App() {
 	const [workerReady, setWorkerReady] = useState(false);
 
 	useEffect(() => {
 		worker.start().then(() => setWorkerReady(true));
-	}, []);
-
-	useEffect(() => {
-		const modalDiv = document.getElementById('modal');
-		if (!modalDiv) {
-			const div = document.createElement('div');
-			div.id = 'modal';
-			document.body.appendChild(div);
-		}
 	}, []);
 
 	if (!workerReady) return null;
