f337e48648fa0bbe0af6a9247f3300723999a375
Sam Choi <dbswo9795@gmail.com>
2024-09-22 12:00:39 +0900

    design: 모달 드래그 추가

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import {
+	motion,
+	AnimatePresence,
+	useDragControls,
+	PanInfo,
+} from 'framer-motion';
 import { Button } from '@/components';
 import { OverlayProps as Props } from '@/libs';
 import { cn } from '@/utils';
@@ -17,6 +22,8 @@ export const Overlay = ({
 	children,
 }: OverlayProps) => {
 	const [isVisible, setIsVisible] = useState(true);
+
+	const controls = useDragControls();
 
 	useEffect(() => {
 		const handleKeyPress = (event: KeyboardEvent) => {
@@ -55,14 +62,20 @@ export const Overlay = ({
 					<motion.div
 						className={cn(
 							'fixed bottom-0 left-0 right-0 z-[9999]',
-							'pt-6 px-[18px] pb-2 max-w-window',
+							'flex flex-col',
+							'pt-2 px-[18px] pb-2 max-w-window',
 							'rounded-t-[28px] bg-white shadow-shadow1',
 						)}
 						initial={{ y: '100%' }}
 						animate={{ y: 0 }}
 						exit={{ y: '100%' }}
 						transition={{ type: 'spring', damping: 25, stiffness: 500 }}
+						drag="y"
+						dragControls={controls}
+						dragConstraints={{ top: 0 }}
+						onDragEnd={() => handleReject()}
 					>
+						<div className="self-center mb-[14px] w-10 h-[3px] rounded-[12px] bg-gray200" />
 						{children}
 						<Button
 							size="sm"
