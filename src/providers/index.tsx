db120a58d00ec667600a906f86f9759715d18ce5
sossost <shagrat8546@gmail.com>
2024-09-22 14:39:10 +0900

    fix : 디버그 로그 제거

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,23 +1,15 @@
 import { ReactNode } from 'react';
 import { QueryProvider } from './QueryProvider';
 import { OverlayContext } from '@/libs';
-import { DebugLog } from '@/components';
-import { useCheckWebview } from '@/hooks';
 
 type Props = {
 	children: ReactNode;
 };
 
 export const ProviderGroup = ({ children }: Props) => {
-	const isWebView = useCheckWebview();
-	const isDevelopment = import.meta.env.MODE === 'development';
-
 	return (
 		<QueryProvider>
-			<OverlayContext>
-				{children}
-				{isWebView && isDevelopment && <DebugLog />}
-			</OverlayContext>
+			<OverlayContext>{children}</OverlayContext>
 		</QueryProvider>
 	);
 };
