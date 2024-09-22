6240412a3fb331571574069c36ac99da245b3f1d
sossost <shagrat8546@gmail.com>
2024-09-22 14:38:40 +0900

    feat : 리액트 쿼리 데브툴즈 추가

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode, useState } from 'react';
 
 type Props = {
@@ -9,6 +10,9 @@ export const QueryProvider = ({ children }: Props) => {
 	const [queryClient] = useState(() => new QueryClient());
 
 	return (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 };
