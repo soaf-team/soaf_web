af489b8e7596f47bf2c193bfd98d00921d804328
H0onnn <dukei2017@naver.com>
2024-09-29 20:47:56 +0900

    design : css 수정

--- src/pages/myHome/main/_components/DeskAndChair.tsx
+++ src/pages/myHome/main/_components/DeskAndChair.tsx
@@ -23,7 +23,7 @@ export const DeskAndChair = (props: Props) => {
 			<img
 				src={DiaryIcon}
 				alt="diary"
-				className="absolute_center ml-[13px] mt-[3px] w-1/4"
+				className="absolute_center w-[36%] ml-[3%] mt-[3px]"
 				onClick={() => push('MyDiaryPage', {})}
 			/>
 		</div>
