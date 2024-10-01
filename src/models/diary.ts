a243ed394f55586f1fc9db725f2539fe56de62a5
sossost <shagrat8546@gmail.com>
2024-10-01 17:54:08 +0900

    fix : 다이어리 타입 수정

--- src/models/diary.ts
+++ src/models/diary.ts
@@ -6,10 +6,12 @@ export function transformDiaryKey(data: DiaryBackend): Diary {
 		authorId: data.userId,
 		title: data.title,
 		content: data.content,
+		rating: data.coreEmotion,
+		isPublic: data.isPublic,
 		photos: data.imageBox,
 		emotions: data.detailedEmotions,
 		date: data.date,
-		reactions: data.reactions,
+		reactions: data.reactions || {},
 		createdAt: data.createdAt,
 		updatedAt: data.updatedAt,
 	};
