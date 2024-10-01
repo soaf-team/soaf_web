e8dd5931df052c7af764d37274ffe8469d8310f9
Sam Choi <dbswo9795@gmail.com>
2024-10-01 17:59:29 +0900

    fix: 데이터 api 형식으로 수정

--- src/pages/chat/room/page.tsx
+++ src/pages/chat/room/page.tsx
@@ -3,50 +3,105 @@ import { useEffect, useLayoutEffect, useRef } from 'react';
 import { BackButton, PageLayout } from '@/components';
 import { MessageInput, SpeechBubble } from './_components';
 import { MyHomeButton } from '../_components';
-import { useSocket } from '@/hooks';
+import { chatSocketManager } from '@/libs';
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
-const DUMMY_CHAT = [
-	{ message: '안녕', sentAt: new Date().toISOString(), userId: 1 },
-	{ message: '안녕하세요', sentAt: new Date().toISOString(), userId: 1 },
-	{ message: '테스트', sentAt: new Date().toISOString(), userId: 1 },
+type ChatMessage = {
+	id: string;
+	senderId: string;
+	type: 'text' | 'image';
+	content: string[];
+	timestamp: string;
+	readBy: string[];
+};
+
+const DUMMY_CHAT: ChatMessage[] = [
 	{
-		message:
+		id: '1',
+		senderId: '1',
+		type: 'text',
+		content: ['안녕'],
+		timestamp: '2024-03-15T09:00:00.000Z',
+		readBy: ['1'],
+	},
+	{
+		id: '2',
+		senderId: '1',
+		type: 'text',
+		content: ['안녕하세요'],
+		timestamp: '2024-03-15T09:01:00.000Z',
+		readBy: ['1'],
+	},
+	{
+		id: '3',
+		senderId: '1',
+		type: 'text',
+		content: ['테스트'],
+		timestamp: '2024-03-15T09:02:00.000Z',
+		readBy: ['1'],
+	},
+	{
+		id: '4',
+		senderId: '2',
+		type: 'text',
+		content: [
 			'네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요네 안녕하세요',
-		nickname: '정훈',
-		sentAt: new Date().toISOString(),
-		userId: 2,
+		],
+		timestamp: '2024-03-15T09:03:00.000Z',
+		readBy: ['1', '2'],
 	},
 	{
-		message: '반갑네요',
-		nickname: '정훈',
-		sentAt: new Date().toISOString(),
-		userId: 2,
+		id: '5',
+		senderId: '2',
+		type: 'image',
+		content: ['/nong1.png'],
+		timestamp: '2024-03-15T09:03:00.000Z',
+		readBy: ['1', '2'],
 	},
 	{
-		message:
+		id: '6',
+		senderId: '1',
+		type: 'text',
+		content: [
 			'까불지 마세요 까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요',
-		sentAt: new Date().toISOString(),
-		userId: 1,
+		],
+		timestamp: '2024-03-15T09:05:00.000Z',
+		readBy: ['1'],
 	},
 	{
-		message:
+		id: '7',
+		senderId: '1',
+		type: 'text',
+		content: [
 			'까불지 마세요 까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요까불지 마세요',
-		sentAt: new Date().toISOString(),
-		userId: 1,
+		],
+		timestamp: '2024-03-15T09:06:00.000Z',
+		readBy: ['1'],
 	},
 	{
-		message: '반갑네요',
-		nickname: '정훈',
-		sentAt: new Date().toISOString(),
-		userId: 2,
+		id: '8',
+		senderId: '2',
+		type: 'text',
+		content: ['반갑네요'],
+		timestamp: '2024-03-15T09:07:00.000Z',
+		readBy: ['1', '2'],
 	},
 	{
-		message: '반갑네요',
-		nickname: '정훈',
-		sentAt: new Date().toISOString(),
-		userId: 2,
+		id: '9',
+		senderId: '2',
+		type: 'text',
+		content: ['반갑네요'],
+		timestamp: '2024-03-15T09:08:00.000Z',
+		readBy: ['1', '2'],
+	},
+	{
+		id: '10',
+		senderId: '2',
+		type: 'image',
+		content: ['/nong1.png', '/nong2.png', '/nong3.png', '/nong4.jpeg'],
+		timestamp: '2024-03-15T09:03:00.000Z',
+		readBy: ['1', '2'],
 	},
 ];
 
@@ -56,11 +111,19 @@ const ChatRoomPage = ({ params }: { params: { userId: string } }) => {
 	const { userId } = params;
 	const chatContainerRef = useRef<HTMLDivElement>(null);
 
-	const { emit } = useSocket(SOCKET_URL);
-
 	useEffect(() => {
-		emit('enterChat', { roomId: String(userId) });
-	}, [userId, emit]);
+		chatSocketManager.connectForPage('ChatRoom');
+		chatSocketManager.on('chatList', (messages: any[]) => {
+			console.log(messages);
+		});
+
+		return () => {
+			chatSocketManager.disconnectForPage('ChatRoom');
+			chatSocketManager.off('chatList', (message) => {
+				console.log(message);
+			});
+		};
+	}, [userId]);
 
 	useLayoutEffect(() => {
 		if (chatContainerRef.current) {
@@ -81,25 +144,28 @@ const ChatRoomPage = ({ params }: { params: { userId: string } }) => {
 			<div ref={chatContainerRef} className="flex-grow overflow-y-auto">
 				<div className="min-h-full flex flex-col">
 					{DUMMY_CHAT.map((data, index, arr) => {
-						const isMine = data.userId === CUSTOMER_ID;
+						const isMine = Number(data.senderId) === CUSTOMER_ID;
 						const isFirst =
-							index === 0 || arr[index - 1].userId !== data.userId;
+							index === 0 || arr[index - 1].senderId !== data.senderId;
 						const currentMessage = arr[index];
 						const nextMessage = arr[index + 1];
 						const isGap =
 							isFirst ||
 							index === arr.length - 1 ||
-							nextMessage.userId === currentMessage.userId
+							nextMessage.senderId === currentMessage.senderId
 								? false
 								: true;
-						const isLast = !nextMessage || nextMessage.userId !== data.userId;
+						const isLast =
+							!nextMessage || nextMessage.senderId !== data.senderId;
 
 						return (
 							<SpeechBubble
 								key={index}
-								message={data.message}
-								sentAt={data.sentAt}
-								nickname={data.nickname}
+								type={data.type}
+								message={data.content}
+								sentAt={data.timestamp}
+								//TODO: 이후에 senderId로 유저 정보 가져오도록 수정 필요
+								nickname={data.senderId}
 								variant={isMine ? 'isMine' : 'isOpponent'}
 								order={
 									isFirst ? (isMine ? 'isFirst' : 'isOpponentFirst') : undefined
