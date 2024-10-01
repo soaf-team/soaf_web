47861e340a5193a9ececbc191544707700e115c5
sossost <shagrat8546@gmail.com>
2024-10-01 20:44:33 +0900

    feat : 이미지 확대 구현

--- src/components/ui/Image.tsx
+++ src/components/ui/Image.tsx
@@ -9,10 +9,7 @@ export const Image = ({ src, ...props }: ImageProps) => {
 	const { push } = useFlow();
 
 	const handleClickImage = () => {
-		const imageId = src.split('/').pop()?.split('.')[0] || '';
-		const encodedImageId = encodeURIComponent(imageId);
-
-		push('ImageDetailPage', { src: encodedImageId, alt: 'image' });
+		push('ImageDetailPage', { src: src, alt: 'image' });
 	};
 
 	return <img onClick={handleClickImage} {...props} src={src} />;
---
+++ src/pages/image/detail/page.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+
+import { PageLayout, XButton } from '@/components';
+import { ActivityComponentType } from '@stackflow/react';
+
+type ImageDetailPageParams = {
+	src: string;
+	alt: string;
+};
+
+const ImageDetailPage: ActivityComponentType<ImageDetailPageParams> = ({
+	params,
+}) => {
+	const { src, alt } = params;
+
+	console.log(src);
+
+	return (
+		<PageLayout
+			header={{
+				rightSlot: <XButton />,
+			}}
+			className="px-0 flex flex-col items-center pb-[56px]"
+		>
+			<img src={src} alt={alt} className="w-full" />
+		</PageLayout>
+	);
+};
+
+export default ImageDetailPage;
+
+ImageDetailPage.displayName = 'ImageDetailPage';
