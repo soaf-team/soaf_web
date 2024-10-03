e3884f0f052843d77b89577c164d3184e2e2187a
Sam Choi <dbswo9795@gmail.com>
2024-10-04 00:11:47 +0900

    fix: 채팅히스토리 스토어 추가

--- src/store/useChatStore.ts
+++ src/store/useChatStore.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 
 import { ChatType, MessageType } from '@/types';
 
+// 전체 채팅방 리스트
 type ChatListStore = {
 	chatList: ChatType[];
 	setChatList: (rooms: ChatType[]) => void;
@@ -10,4 +11,15 @@ type ChatListStore = {
 export const useChatListStore = create<ChatListStore>((set) => ({
 	chatList: [],
 	setChatList: (chat) => set({ chatList: chat }),
+}));
+
+// 현재 입장 채팅방
+type CurrentChatType = {
+	chatHistoryList: MessageType[];
+	setChatHistoryList: (chat: MessageType[]) => void;
+};
+
+export const useCurrentChatStore = create<CurrentChatType>((set) => ({
+	chatHistoryList: [],
+	setChatHistoryList: (chat) => set({ chatHistoryList: chat }),
 }));
