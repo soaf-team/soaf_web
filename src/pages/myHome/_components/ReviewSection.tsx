157eaf77dbfdf238866ffa6c661b621549cb6372
H0onnn <dukei2017@naver.com>
2024-10-06 01:39:20 +0900

    feat : readonly props 추가

--- src/pages/myHome/_components/ReviewSection.tsx
+++ src/pages/myHome/_components/ReviewSection.tsx
@@ -9,6 +9,7 @@ interface Props {
 	value: string;
 	maxLength?: number;
 	onChange: (value: string) => void;
+	readOnly?: boolean;
 }
 
 export const ReviewSection = ({
@@ -18,6 +19,7 @@ export const ReviewSection = ({
 	value,
 	maxLength = 1000,
 	onChange,
+	readOnly,
 }: Props) => {
 	const textareaRef = useRef<HTMLTextAreaElement>(null);
 
@@ -49,6 +51,7 @@ export const ReviewSection = ({
 					className,
 				)}
 				onInput={autoResizeTextarea}
+				readOnly={readOnly}
 			/>
 
 			<div className="w-full h-px bg-border" />
