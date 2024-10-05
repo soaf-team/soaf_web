7c042e5d3c634cf776f61a884fbe8e733e026fc1
H0onnn <dukei2017@naver.com>
2024-10-05 14:08:33 +0900

    remove : 더미데이터 삭제

--- src/hooks/queries/myHome/music/useMyMusicsQuery.ts
+++ src/hooks/queries/myHome/music/useMyMusicsQuery.ts
@@ -25,37 +25,5 @@ export const useMyMusicListQuery = (userId: string) => {
 		queryFn: fetchMyMusicList,
 	});
 
-	return { myMusicList, isFetching, MOCK_MY_MUSIC_LIST };
+	return { myMusicList, isFetching };
 };
-
-const MOCK_MY_MUSIC_LIST: MyMusic[] = [
-	{
-		id: 1,
-		title: 'title1',
-		artist: 'artist1',
-		thumbnailUrl:
-			'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-		review: '개똥같은 노래여서 별로에요',
-	},
-	{
-		id: 2,
-		title: 'title2',
-		artist: 'artist2',
-		thumbnailUrl:
-			'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-	},
-	{
-		id: 3,
-		title: 'title3',
-		artist: 'artist3',
-		thumbnailUrl:
-			'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-	},
-	{
-		id: 4,
-		title: 'title4',
-		artist: 'artist4',
-		thumbnailUrl:
-			'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-	},
-];
