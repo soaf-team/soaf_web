3cc9a0f3c2a5f7a9a2116da2b2ea1b4725a39333
sossost <shagrat8546@gmail.com>
2024-10-17 20:54:43 +0900

    fix : 감정 타입 수정

--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -1,19 +1,36 @@
 export type EmotionKey =
-	| '행복한'
-	| '기분좋은'
-	| '즐거운'
-	| '설레는'
-	| '뿌듯한'
-	| '편안한'
-	| '피곤한'
-	| '외로운'
-	| '슬픈'
-	| '우울한'
-	| '불안한'
-	| '화난';
+	| 'happy'
+	| 'good'
+	| 'joyful'
+	| 'excited'
+	| 'proud'
+	| 'calm'
+	| 'tired'
+	| 'lonely'
+	| 'sad'
+	| 'down'
+	| 'worried'
+	| 'angry';
 
 export type EmotionDetail = {
 	icon: string;
 	color: string;
 	noun: string;
+	label: string;
+	value: EmotionKey;
 };
+
+const enum Emotion {
+	happy = '행복한',
+	good = '기분좋은',
+	joyful = '즐거운',
+	excited = '설레는',
+	proud = '뿌듯한',
+	calm = '편안한',
+	tired = '피곤한',
+	lonely = '외로운',
+	sad = '슬픈',
+	down = '우울한',
+	worried = '불안한',
+	angry = '화난',
+}
