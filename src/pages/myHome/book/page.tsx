21d9a66c89d51aaa24327a1055218f1eda9a2ab0
H0onnn <dukei2017@naver.com>
2024-10-20 18:49:53 +0900

    feat : 나의 도서목록 페이지 api 연결

--- src/pages/myHome/book/page.tsx
+++ src/pages/myHome/book/page.tsx
@@ -1,11 +1,26 @@
 import { BackButton, PageLayout, PlusButton } from '@/components';
-import { MyHomeDrawer } from '../_components';
+import { MyHomeDrawer, MyItem } from '../_components';
 import { RegisterBookForm } from './_components/RegisterBookForm';
 import { overlay } from '@/libs';
+import { useUserProfileQuery, useMyBooksQuery } from '@/hooks';
+import { useFlow } from '@/stackflow';
+import { cn } from '@/utils';
 
 const MyBookPage = () => {
+	const { push } = useFlow();
+	const { userProfile } = useUserProfileQuery();
+	const { myBookList, isFetching } = useMyBooksQuery(userProfile?.id);
+
+	// TODO: fetching skeleton
+
 	const handleOpenOverlay = async () => {
 		await overlay.open(<MyHomeDrawer component={<RegisterBookForm />} />);
+	};
+
+	const handleClickBookItem = (bookId: string) => {
+		push('MyBookDetailPage', {
+			bookId,
+		});
 	};
 
 	return (
@@ -16,9 +31,34 @@ const MyBookPage = () => {
 				rightSlot: <PlusButton onClick={handleOpenOverlay} />,
 			}}
 		>
-			<div className="flex flex-col gap-[8px] justify-center items-center h-full body2m text-gray200">
-				<p>좋아하는 도서를 추가해</p>
-				<p>나만의 취향 목록을 만들어보세요</p>
+			<div
+				className={cn(
+					'flex flex-col pt-[56px] h-full',
+					myBookList?.data.length === 0 && 'p-0',
+				)}
+			>
+				{myBookList?.data.length === 0 ? (
+					<div className="flex flex-col gap-[8px] justify-center items-center h-full body2m text-gray200">
+						<p>좋아하는 도서를 추가해</p>
+						<p>나만의 취향 목록을 만들어보세요</p>
+					</div>
+				) : (
+					<div className="flex flex-col gap-[14px] items-center justify-center">
+						<p className="text-gray300 label3">
+							총 {myBookList?.data.length}개의 도서
+						</p>
+
+						<div className="grid w-full grid-cols-3 gap-2">
+							{myBookList?.data.map((book) => (
+								<MyItem
+									key={book._id}
+									item={book}
+									onClick={() => handleClickBookItem(book._id)}
+								/>
+							))}
+						</div>
+					</div>
+				)}
 			</div>
 		</PageLayout>
 	);
