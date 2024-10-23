cd2095746148f7aa7727bbb2a90b7b7e923e3979
sossost <shagrat8546@gmail.com>
2024-10-23 14:11:00 +0900

    fix : 베이스 url변경

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -3,7 +3,8 @@ import { refreshAccessToken } from './refreshAccessToken';
 import { sendMessageToApp } from '@/utils';
 
 export const axiosBase = axios.create({
-	baseURL: 'api/',
+	baseURL:
+		'http://default-api-service-77559-26019998-ded2c8967f62.kr.lb.naverncp.com:8443/api/',
 	headers: {
 		Authorization: 'Bearer api_secret_soap',
 	},
