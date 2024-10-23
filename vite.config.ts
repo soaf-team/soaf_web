f4774a57ae339231b47ff938c0ec000282046477
sossost <shagrat8546@gmail.com>
2024-10-23 13:34:18 +0900

    fix : 프록시 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [react(), tsconfigPaths()],
+	base: '/',
+	plugins: [react(), tsconfigPaths()],
+	server: {
+		proxy: {
+			'/api': {
+				target:
+					'http://default-api-service-77559-26019998-ded2c8967f62.kr.lb.naverncp.com:8443',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
+		},
+	},
 });
