2289b7935062b8c742e3921fd58b46d981716bc6
H0onnn <dukei2017@naver.com>
2024-10-25 21:38:40 +0900

    feat : 나의 유튜브 목록 api 연결

--- src/pages/myHome/youtube/page.tsx
+++ src/pages/myHome/youtube/page.tsx
@@ -2,10 +2,24 @@ import { BackButton, PageLayout, PlusButton } from '@/components';
 import { MyHomeDrawer } from '../_components';
 import { RegisterYoutubeForm } from './_components/RegisterYoutubeForm';
 import { overlay } from '@/libs';
+import { useFlow } from '@/stackflow';
+import { useMyYoutubesQuery, useUserProfileQuery } from '@/hooks';
+import { cn } from '@/utils';
+import { YoutubeItem } from './_components/YoutubeItem';
 
 const MyYoutubePage = () => {
+	const { push } = useFlow();
+	const { userProfile } = useUserProfileQuery();
+	const { myYoutubeList, isFetching } = useMyYoutubesQuery(userProfile?.id);
+
 	const handleOpenOverlay = async () => {
 		await overlay.open(<MyHomeDrawer component={<RegisterYoutubeForm />} />);
+	};
+
+	const handleClickYoutubeItem = (youtubeId: string) => {
+		push('MyYoutubeDetailPage', {
+			youtubeId,
+		});
 	};
 
 	return (
@@ -16,9 +30,33 @@ const MyYoutubePage = () => {
 				rightSlot: <PlusButton onClick={handleOpenOverlay} />,
 			}}
 		>
-			<div className="flex flex-col gap-[8px] justify-center items-center h-full body2m text-gray200">
-				<p>좋아하는 유튜브 링크를 추가해</p>
-				<p>나만의 취향 목록을 만들어보세요</p>
+			<div
+				className={cn(
+					'flex flex-col pt-[56px] h-full',
+					myYoutubeList?.data.length === 0 && 'p-0',
+				)}
+			>
+				{myYoutubeList?.data.length === 0 ? (
+					<div className="flex flex-col gap-[8px] justify-center items-center h-full body2m text-gray200">
+						<p>좋아하는 유튜브 링크를 추가해</p>
+						<p>나만의 취향 목록을 만들어보세요</p>
+					</div>
+				) : (
+					<div className="flex flex-col gap-[14px] items-center justify-center">
+						<p className="text-gray300 label3">
+							총 {myYoutubeList?.data.length}개의 영상
+						</p>
+
+						{myYoutubeList?.data.map((youtube) => (
+							<YoutubeItem
+								key={youtube._id}
+								type="list"
+								youtube={youtube}
+								onClick={() => handleClickYoutubeItem(youtube._id)}
+							/>
+						))}
+					</div>
+				)}
 			</div>
 		</PageLayout>
 	);
