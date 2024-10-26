c1beb6f19bcf9febe462c8f61c6f90304f417f76
sossost <shagrat8546@gmail.com>
2024-10-26 20:48:13 +0900

    fix : 백엔드 api url 수정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -3,8 +3,7 @@ import { refreshAccessToken } from './refreshAccessToken';
 import { sendMessageToApp } from '@/utils';
 
 export const axiosBase = axios.create({
-	baseURL:
-		'http://default-api-service-77559-26019998-ded2c8967f62.kr.lb.naverncp.com:8443/',
+	baseURL: 'https://soaf.me/',
 	headers: {
 		Authorization: 'Bearer api_secret_soap',
 	},
