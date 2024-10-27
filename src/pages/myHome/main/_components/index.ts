21d70df313aba95343dca05f4c4b285f4762b86a
H0onnn <dukei2017@naver.com>
2024-10-27 18:01:40 +0900

    feat : 소프 신청 오버레이 컴포넌트 추가

--- src/pages/myHome/main/_components/index.ts
+++ src/pages/myHome/main/_components/index.ts
@@ -4,3 +4,5 @@ export * from './Soaf';
 export * from './UpButton';
 export * from './BottomActionButtons';
 export * from './DeskAndChair';
+export * from './SoafStatusBadge';
+export * from './SoafRequestDialogOverlay';
