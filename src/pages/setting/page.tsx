3b18f2a453547590b594e297bb950455cdbe41b9
H0onnn <dukei2017@naver.com>
2024-10-28 23:32:29 +0900

    feat : 임시 알림 토글 핸들러 추가

--- src/pages/setting/page.tsx
+++ src/pages/setting/page.tsx
@@ -1,4 +1,5 @@
-import { useUserProfileQuery } from '@/hooks';
+import { useState } from 'react';
+import { useToast, useUserProfileQuery } from '@/hooks';
 import { useSettingMenuItems } from './hooks';
 import { BackButton, Divider, PageLayout, Switch } from '@/components';
 import { ActivityComponentType } from '@stackflow/react';
@@ -7,8 +8,25 @@ import { MenuList, UserNameSection } from './_components';
 // TODO: 알림 로직, 앱 버전 추가
 
 const SettingPage: ActivityComponentType = () => {
+	const { toast } = useToast();
 	const { userProfile } = useUserProfileQuery();
 	const { MENU_ITEMS1, MENU_ITEMS2 } = useSettingMenuItems();
+	const [isNotificationOn, setIsNotificationOn] = useState(true);
+
+	const handleNotificationChange = () => {
+		if (isNotificationOn) {
+			setIsNotificationOn(false);
+			toast({
+				title: 'PUSH 알림이 해제되었어요',
+			});
+		} else {
+			setIsNotificationOn(true);
+			toast({
+				title: 'PUSH 알림이 설정되었어요',
+			});
+		}
+	};
+
 	return (
 		<PageLayout
 			header={{
@@ -26,7 +44,11 @@ const SettingPage: ActivityComponentType = () => {
 
 			<div className="w-full flex items-center justify-between py-[22px]">
 				<span className="text-black">알림 설정</span>
-				<Switch defaultChecked={true} onCheckedChange={() => {}} checked />
+				<Switch
+					defaultChecked={true}
+					onCheckedChange={handleNotificationChange}
+					checked={isNotificationOn}
+				/>
 			</div>
 
 			<Divider className="bg-gray50" />
