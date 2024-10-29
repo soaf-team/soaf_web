f8af9df39f4d8d2369f39f0e4104e1bcf5e1fe31
sossost <shagrat8546@gmail.com>
2024-10-29 20:46:24 +0900

    feat : 404사 반환시 로그아웃 로직 추가

--- src/hooks/queries/user/useUserProfileQuery.ts
+++ src/hooks/queries/user/useUserProfileQuery.ts
@@ -3,10 +3,23 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import { axiosBase } from '@/apis';
 import { QUERY_KEY } from '@/constants';
 import { User } from '@/types';
+import { AxiosError } from 'axios';
+import { sendMessageToApp } from '@/utils';
 
 const getUserProfile = async () => {
-	const response = await axiosBase.get('user/profile');
-	return response.data.data;
+	try {
+		const response = await axiosBase.get('user/profile');
+		return response.data.data;
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			if (error.response?.status === 404) {
+				sendMessageToApp({
+					type: 'LOGOUT',
+				});
+			}
+		}
+		throw error;
+	}
 };
 
 export const useUserProfileQuery = () => {
