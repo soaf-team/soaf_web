64ff4153d9ca8ddef6b187de9288ebc60e97487d
Sam Choi <dbswo9795@gmail.com>
2024-10-31 02:04:34 +0900

    fix: 상태메시지 props로 넘김

--- src/pages/chat/_components/StatusMessageOverlay.tsx
+++ src/pages/chat/_components/StatusMessageOverlay.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import { ConfirmOverlay } from '@/components/overlay';
 import { Input } from '@/components/ui';
 import { OverlayProps } from '@/libs';
 
-export const StatusMessageOverlay = ({ resolve, reject }: OverlayProps) => {
-	const [statusMessage, setStatusMessage] = useState('');
+export const StatusMessageOverlay = ({
+	userStatus,
+	resolve,
+	reject,
+}: OverlayProps & { userStatus: string | null }) => {
+	const [statusMessage, setStatusMessage] = useState(userStatus ?? '');
+	const inputRef = useRef<HTMLInputElement>(null);
 
 	const handleChange = (value: string) => {
 		setStatusMessage(value);
@@ -20,6 +25,7 @@ export const StatusMessageOverlay = ({ resolve, reject }: OverlayProps) => {
 		>
 			<h2 className="py-3 font-semibold text-center">상태메시지 올리기</h2>
 			<Input
+				ref={inputRef}
 				className="h-[28px] py-1"
 				isResetButton
 				maxLength={20}
