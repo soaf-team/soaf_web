31836dba6f73e08108dccbd7fb7bdef828894dea
Sam Choi <dbswo9795@gmail.com>
2024-10-30 16:03:53 +0900

    fix: 다이얼로그 height 오토로 수정

--- src/pages/notification/_components/SoafRequestNofiyOverlay.tsx
+++ src/pages/notification/_components/SoafRequestNofiyOverlay.tsx
@@ -46,7 +46,7 @@ export const SoafRequestNotifyOverlay = ({
 			/>
 			<dialog
 				className={cn(
-					'fixed left-[50%] top-[50%] z-50 grid translate-x-[-50%] w-[calc(100%-44px)] translate-y-[-50%] gap-[24px] bg-white rounded-[20px] px-[12px] pt-[34px] pb-[20px] shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+					'fixed left-[50%] top-[50%] z-50 grid translate-x-[-50%] w-[calc(100%-44px)] translate-y-[-50%] min-h-0 h-auto gap-[24px] bg-white rounded-[20px] px-[12px] pt-[34px] pb-[20px] shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
 					className,
 				)}
 			>
