dbba43bdefe28a1a8e18f0fa0e623101ecf3f67c
H0onnn <dukei2017@naver.com>
2024-10-31 22:12:41 +0900

    fix : 마이홈 꾸미기 후 invalidate 안되는 부분 수정

--- src/hooks/mutations/useInteriorPositionMutations.tsx
+++ src/hooks/mutations/useInteriorPositionMutations.tsx
@@ -1,6 +1,8 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { useGenericMutation } from './useGenericMutation';
 import { useToast } from '../useToast';
 import { Interior } from '@/types';
+import { QUERY_KEY } from '@/constants';
 
 export interface PositionPayloadType {
 	items: Omit<Interior, 'type' | '_id'>[];
@@ -8,6 +10,7 @@ export interface PositionPayloadType {
 
 export const useInteriorPositionMutations = () => {
 	const { toast } = useToast();
+	const queryClient = useQueryClient();
 
 	const updatePositionMutation = useGenericMutation<
 		void,
@@ -15,6 +18,7 @@ export const useInteriorPositionMutations = () => {
 		PositionPayloadType
 	>('/room', 'PATCH', {
 		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: [QUERY_KEY.MY_HOME_DATA] });
 			toast({
 				title: '마이홈 꾸미기가 수정되었어요',
 			});
