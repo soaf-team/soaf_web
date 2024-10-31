d116d9be7a2173df77ce3a43aecb3db459458b4e
Sam Choi <dbswo9795@gmail.com>
2024-10-31 17:37:58 +0900

    fix: http -> https로 수정

--- src/hooks/queries/myHome/music/useMusicDetailQuery.ts
+++ src/hooks/queries/myHome/music/useMusicDetailQuery.ts
@@ -11,7 +11,7 @@ export const useMusicDetailQuery = ({ name, artist }: Props) => {
 	const fetchMusic = async () => {
 		try {
 			const response = await axios.get(
-				`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${import.meta.env.VITE_LAST_FM_API_KEY}&artist=${artist}&album=${name}&format=json`,
+				`https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${import.meta.env.VITE_LAST_FM_API_KEY}&artist=${artist}&album=${name}&format=json`,
 			);
 
 			return response.data;
